3f74411e92cf21b500bd3c2846d7024e75f1b532
Stephen Sherratt <sfsherratt@gmail.com>
2019-01-12 10:17:27 +0000

    Deterministic wasm module name in output

--- wasm/webpack.config.js
+++ wasm/webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
+        webassemblyModuleFilename: "app.wasm",
     },
     plugins: [
         new CopyWebpackPlugin([{ from: "static_web" }]),
