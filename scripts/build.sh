cd5bc7aeb565ce10d8f52e77b0ddcc92b31ebf03
Stephen Sherratt <sfsherratt@gmail.com>
2019-01-12 11:06:59 +0000

    Only deploy wasm on linux, re-order local test to match travis test

--- scripts/build.sh
+++ scripts/build.sh
@@ -65,8 +65,8 @@ case $TRAVIS_OS_NAME in
         $BUILD_PY_COMMON --os=macos --frontend=unix --frontend=glutin --crate-path=$UNIX_CRATE --crate-path=$GLUTIN_CRATE
         ;;
     local-archlinux)
-        $BUILD_PY_COMMON --os=linux --frontend=unix --frontend=glutin --crate-path=$UNIX_CRATE --crate-path=$GLUTIN_CRATE
         $BUILD_PY --root-path=$PROJECT_ROOT --build-path=$BUILD_DIR --upload-path=$WEB_UPLOAD_DIR \
             --os=unknown --frontend=wasm --crate-path=$WASM_CRATE
+        $BUILD_PY_COMMON --os=linux --frontend=unix --frontend=glutin --crate-path=$UNIX_CRATE --crate-path=$GLUTIN_CRATE
         ;;
 esac
